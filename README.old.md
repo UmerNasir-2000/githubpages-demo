4eddd35fe0122862c617ada79222b7d8b937dbfb
Umer Nasir <umer.nas@outlook.com>
2022-01-11 12:10:59 +0500

    API FETCHED

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# githubpages-demo
-This repository is to demonstrate the API on heroku fetched with create-react-app deployed on GitHub Pages.
--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await axios(
+        "https://shopit-api-legendary.herokuapp.com/api/products/fetchProducts"
+      );
+
+      console.log(response.data);
+      setProducts(response.data);
+    };
+
+    fetchProducts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>ALL PRODUCTS</h1>
+      <div>
+        {products.map((product) => (
+          <div key={product._id}>{product.name}</div>
+        ))}
+      </div>
+    </>
   );
 }
 
